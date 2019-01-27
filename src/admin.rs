de314603e7da94a4496d05207a57321f6c0df106
Duncan Dean <duncangleeddean@gmail.com>
2019-01-27 18:15:27 +0200

    convert all &'a str to String

--- src/admin.rs
+++ src/admin.rs
@@ -6,14 +6,14 @@ use super::credentials;
 
 
 #[derive(Clone)]
-pub struct Admin<'a> {
-    pub apps: Vec<app::App<'a>>,
+pub struct Admin {
+    pub apps: Vec<app::App>,
 }
 
 // TODO: Initialize SDK with google-service.json
-impl<'a> Admin<'a> {
+impl Admin {
     /// Constructs an `AppBuilder` struct
-    pub fn app_builder() -> AppBuilder<'a> {
+    pub fn app_builder() -> AppBuilder<String> {
         
         AppBuilder {
             name: None,
@@ -32,29 +32,29 @@ impl<'a> Admin<'a> {
 
 /// Allows you to build your options for your app before it is 
 /// initialized.
-pub struct AppBuilder<'a> {
-    name: Option<&'a str>,
+pub struct AppBuilder<String> {
+    name: Option<String>,
     credentials: Option<credentials::Credentials>,
-    database_auth_variable_override: Option<HashMap<&'a str, &'a str>>,
-    database_url: Option<&'a str>,
-    http_agent: Option<HashMap<&'a str, &'a str>>,
-    project_id: Option<&'a str>,
-    service_account_id: Option<&'a str>,
-    storage_bucket: Option<&'a str>
+    database_auth_variable_override: Option<HashMap<String, String>>,
+    database_url: Option<String>,
+    http_agent: Option<HashMap<String, String>>,
+    project_id: Option<String>,
+    service_account_id: Option<String>,
+    storage_bucket: Option<String>
 }
 
-impl<'a> AppBuilder<'a> {
+impl AppBuilder<String> {
 
     // Optionally set the name of your app. Defaults to `"[DEFAULT]"`.
-    pub fn with_name(mut self, name: &'a str) -> Self {
-        self.name = Some(name);
+    pub fn with_name(mut self, name: &str) -> Self {
+        self.name = Some(name.to_string());
         self
     }
 
     /// Optionally specify the location of your app credentials JSON file.
     /// Defaults to looking for Application Default Credentials and panics if
     /// they cannot be found.
-    pub fn with_credentials(mut self, credentials_file: &'a str) -> Self {
+    pub fn with_credentials(mut self, credentials_file: &str) -> Self {
         let credentials = credentials::Credentials::from_file(credentials_file);
         println!("{}", credentials.project_id);
         self.credentials = Some(credentials);
@@ -63,53 +63,53 @@ impl<'a> AppBuilder<'a> {
 
     /// Optionally set the Database URL for your Firebase project.
     /// Defaults to `[project-id].firebaseio.com`
-    pub fn with_database_url(mut self, database_url: &'a str) -> Self {
-        self.database_url = Some(database_url);
+    pub fn with_database_url(mut self, database_url: &str) -> Self {
+        self.database_url = Some(database_url.to_string());
         self
     }
 
     /// Optionally set the Project ID for the Firebase project you are
     /// using. Defaults to the Project ID specified in your credentials
     /// file.
-    pub fn with_project_id(mut self, project_id: &'a str) -> Self {
-        self.project_id = Some(project_id);
+    pub fn with_project_id(mut self, project_id: &str) -> Self {
+        self.project_id = Some(project_id.to_string());
         self
     }
 
-    pub fn with_http_agent(mut self, http_agent: HashMap<&'a str, &'a str>) -> Self {
+    pub fn with_http_agent(mut self, http_agent: HashMap<String, String>) -> Self {
         self.http_agent = Some(http_agent);
         self
     }
 
-    pub fn with_service_account_id(mut self, service_account_id: &'a str) -> Self {
-        self.service_account_id = Some(service_account_id);
+    pub fn with_service_account_id(mut self, service_account_id: &str) -> Self {
+        self.service_account_id = Some(service_account_id.to_string());
         self
     }
 
     /// Optionally set the Storage Bucket URL for your Firebase project.
     /// Defaults to `[project-id].appspot.com`.
-    pub fn with_storage_bucket(mut self, storage_bucket: &'a str) -> Self {
-        self.storage_bucket = Some(storage_bucket);
+    pub fn with_storage_bucket(mut self, storage_bucket: &str) -> Self {
+        self.storage_bucket = Some(storage_bucket.to_string());
         self
     }
 
     /// Completes the build and initializes the app
-    pub fn build_and_initialize(self) -> Result<app::App<'a>, String> {
+    pub fn build_and_initialize(self) -> Result<app::App, String> {
         let credentials = self.credentials.unwrap().clone();
         let default_project_id = &credentials.project_id.clone();
-        let project_id = self.project_id.unwrap_or(default_project_id);
+        let project_id = self.project_id.unwrap_or(default_project_id.to_string());
         println!("{}", credentials.project_id);
 
         let built_app = app::App {
-            name: self.name.unwrap_or("[DEFAULT]"),
+            name: self.name.unwrap_or("[DEFAULT]".to_string()),
             options: app::AppOptions {
                 credentials: credentials.clone(),
-                database_url: String::from(self.database_url.unwrap_or(&format!("{}.firebaseio.com", &project_id.clone()))),
+                database_url: self.database_url.unwrap_or(format!("{}.firebaseio.com", &project_id.clone())),
                 database_auth_variable_override: self.database_auth_variable_override.unwrap_or(HashMap::new()),
                 http_agent: self.http_agent.unwrap_or(HashMap::new()),
-                project_id: String::from(project_id),
-                service_account_id: String::from(self.service_account_id.unwrap_or(&credentials.clone().client_id)),
-                storage_bucket: String::from(self.storage_bucket.unwrap_or(&format!("{}.appspot.com", &project_id.clone())))
+                project_id: project_id.to_string(),
+                service_account_id: String::from(self.service_account_id.unwrap_or(credentials.client_id)),
+                storage_bucket: String::from(self.storage_bucket.unwrap_or(format!("{}.appspot.com", project_id)))
             }
         };
 
